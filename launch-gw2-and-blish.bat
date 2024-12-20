c1bbc26422b6811ab9155c3c907ea42fe36b907a
Denis Stashko <stashkodenis@gmail.com>
2024-12-20 15:24:42 +0300

    .bat won't read cyrillic - appended a workaround

--- launch-gw2-and-blish.bat
+++ launch-gw2-and-blish.bat
@@ -1,3 +1,7 @@
 @echo off
+
+REM Set the code page to support Cyrillic characters
+CHCP 65001 > nul
+
 start "" "C:\games\Guild Wars 2\Gw2-64.exe"
 start "" "C:\Users\Денис\games\gw2\blishud\Blish HUD.exe"
