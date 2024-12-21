e94d6e3167f45d3b1ea10ea76bc716fab443677d
Denis Stashko <stashkodenis@gmail.com>
2024-12-21 13:31:23 +0300

    add launch docker to script to mount

---
+++ mount-ext4-drives-and-launch-docker.ps1
@@ -0,0 +1,4 @@
+wsl --mount \\.\PHYSICALDRIVE0 --partition 1 --type ext4 # newest
+wsl --mount \\.\PHYSICALDRIVE1 --partition 1 --type ext4 # 1123
+
+"C:\Program Files\Docker\Docker\Docker Desktop.exe"
--- mount-ext4-drives.ps1
+++
@@ -1,2 +0,0 @@
-wsl --mount \\.\PHYSICALDRIVE0 --partition 1 --type ext4 # newest
-wsl --mount \\.\PHYSICALDRIVE1 --partition 1 --type ext4 # 1123
