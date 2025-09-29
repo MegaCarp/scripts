078fb6a5096cb7a5911d0a9da955ba7b6c4a62ac
Denis Stashko <stashkodenis@gmail.com>
2025-09-29 22:20:58 +0300

    save

--- ahk/gw2/InputBinds/forloop_hardlinks.ps1
+++ ahk/gw2/InputBinds/forloop_hardlinks.ps1
@@ -4,7 +4,7 @@ $fileNames = (Get-ChildItem -Path $sourcePath -File).Name
 
 foreach ($xml in $fileNames) {
     $whereToPutLink = "C:\Users\stash\Documents\scripts\ahk\gw2\InputBinds\" + $xml
-    $LinkFrom = "C:\Users\stash\Documents\Guild Wars 2\InputBinds\" + $xml
+    $LinkFrom = "C:\Users\stash\AppData\Roaming\Gw2Launcher\data\1\Documents\Guild Wars 2\InputBinds\" + $xml
 
     New-Item -ItemType HardLink -Path $whereToPutLink -Target $LinkFrom
 }
